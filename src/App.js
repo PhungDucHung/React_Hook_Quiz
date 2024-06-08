c3e377ff77d9a1c807454c43a4a3016ee8544809
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-08 18:17:37 +0700

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.scss';
-
+import Header from './component/Header/Header';
+import { Outlet ,NavLink } from 'react-router-dom';
 
 const App = () => {
+
   return (
     <div className="app-container">
-        hello
+      <div className='header-container'>
+          <Header/>
+      </div>
+
+      <div className='main-container'>
+          <div className='sidenav-container'>
+
+         </div>
+      </div>
+    
+      <div className='app-content'>
+         <Outlet />
+      </div>
     </div>
   );
 }
---
+++ src/component/Home/HomePage.js
@@ -0,0 +1,8 @@
+const HomePage = (props) => {
+    return(
+        <div className="homepage-container">
+            <div>home page component</div>
+        </div>
+    )
+}
+export default HomePage;
