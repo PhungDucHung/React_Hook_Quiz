de8418516cb66d8fdc870bc2a6f8ce3e87f59aad
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-12 00:29:52 +0700

    Validate and Toastify

--- src/component/Admin/content/ModalCreateUser.js
+++ src/component/Admin/content/ModalCreateUser.js
@@ -4,6 +4,8 @@ import Modal from 'react-bootstrap/Modal';
 import './ManageUser.scss';
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 const ModalCreateUser = (props) => {
   const {show ,setShow} = props;
@@ -36,17 +38,28 @@ const ModalCreateUser = (props) => {
     }
   }
 
-  const handSubmitCreateUser = async() => {
-    //  let data = {
-    //   email: email,
-    //   password: password,
-    //   username: Username,
-    //   role: role,
-    //   userImage: image,
-    //  }
-    //  console.log(data);
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
+  const handSubmitCreateUser = async() => {
     //---------------- copy code trên trang github axio
+    // validate
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      toast.warning('invalid email')
+      return;
+    }
+    if (!password) {
+      toast.warning('invalid password')
+      return;
+    }
+
+      //submit data
      const data = new FormData();
       data.append('email', email);
       data.append('password', password);
@@ -55,7 +68,14 @@ const ModalCreateUser = (props) => {
       data.append('userImage', image);
       
       let res = await axios.post('http://localhost:8081/api/v1/participant', data)
-      console.log("check res", res);
+      // console.log("check res", res.data);
+      if(res.data && res.data.EC === 0){
+        toast.success('Add Success')
+        handleClose();
+      }
+      if(res.data && res.data.EC !== 0){
+        toast.error('Invalid');
+      }
   }
     //-------------------------------------------------
   return (
