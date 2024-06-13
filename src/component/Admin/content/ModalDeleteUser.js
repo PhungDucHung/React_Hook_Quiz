0ac7fd8ca249897f17060653560248ef8fefbd6b
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-13 22:33:01 +0700

    APIs Delete User

--- src/component/Admin/content/ModalDeleteUser.js
+++ src/component/Admin/content/ModalDeleteUser.js
@@ -1,13 +1,23 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { deleteUser } from '../../../services/apiService';
+import { toast } from "react-toastify";
 
 const ModalDeleteUser = (props) => {
-  const {show, setShow , dataDelete} = props;
-  const handleClose = () => setShow(false);
-  const handleSubmitDeleteUser = () => {
-    alert('me')
-  }
+      const {show, setShow , dataDelete} = props;
+      const handleClose = () => setShow(false);
+      const handleSubmitDeleteUser = async() => {
+        let data = await deleteUser(dataDelete.id);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
+            handleClose();
+            await props.fetchListUsers(); // Gọi hàm fetchListUsers để cập nhật danh sách người dùng
+        } else {
+            toast.error("Error: " + data.EM);
+        }
+    }
+
   return (
     <>
       <Modal show={show} onHide={handleClose} backdrop="static">
