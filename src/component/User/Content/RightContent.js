84a4408ca3e523f0bad109662b663d3f6c733339
Phung Duc Hung <phungduchung9808@gmail.com>
2024-06-23 13:48:24 +0700

    Countdown Timer

--- src/component/User/Content/RightContent.js
+++ src/component/User/Content/RightContent.js
@@ -1,16 +1,30 @@
+import { indexOf } from "lodash";
+import CountDown from "./CountDown";
+
+
 const RightContent = (props) => {
-    const {dataQuiz} = props;
-    
+const {dataQuiz} = props;
+
+const onTimeUp = () => {
+    props.handleFinishQuiz();
+}
     return (
         <>
             <div className="main-timer">
-                10:10
+                <CountDown
+                    onTimeUp={onTimeUp}
+                />
             </div>
             <div className="main-question">
                 {dataQuiz && dataQuiz.length > 0 
                     && dataQuiz.map((item, index) => {
                         return (
-                            <div className="question">{index + 1}</div>
+                            <div 
+                            className="question"
+                            key={`question-abc-${index}`}
+                            >
+                                
+                            {index + 1}</div>
                         )
                     })
                 }
--- src/component/User/DetailQuiz.js
+++ src/component/User/DetailQuiz.js
@@ -161,6 +161,7 @@ const DetailQuiz = (props) => {
             <div className="right-content">
                 <RightContent
                     dataQuiz = {dataQuiz}
+                    handleFinishQuiz = {handleFinishQuiz}
                 />
             </div>
             <ModalResult
